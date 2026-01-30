5ee6ee87130e53374931a62bbe70aae93bd3c96c
Swifttap <swifttap1@gmail.com>
2026-01-30 00:32:27 -0500

    added adsense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,15 +9,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        {/* Google AdSense */}
+      <head>
+        {/* Google AdSense Auto Ads */}
         <Script
           async
-          strategy="afterInteractive"
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXX"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9936910832077538"
           crossOrigin="anonymous"
+          strategy="afterInteractive"
         />
+      </head>
 
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
